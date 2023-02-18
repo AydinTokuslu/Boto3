4ddbd79c6cf8da576a793f2fcc30945550359e62
aydin <aydintokuslu78@gmail.com>
2023-02-18 21:58:47 +0300

    18.02.23

--- ec2-collections.py
+++ ec2-collections.py
@@ -6,10 +6,16 @@ ec2 = boto3.resource("ec2")
 #     print("Instance id is {} and Instance type is {} ".format(instance.instance_id, instance.instance_type))
 #     # Instance id is i-0bed83164f56d3eb9 and Instance type is t2.micro 
 
-#  filter uygularsak
-for instance in ec2.instances.filter(Filters=[{
-    'Name': 'availability-zone',
-    'Values': ['us-east-1e']
-}]):
-    print("Instance id is {} and Instance type is {} ".format(instance.instance_id, instance.instance_type))
-    # Instance id is i-0bed83164f56d3eb9 and Instance type is t2.micro 
+
+#  availability-zone ile filter uygularsak
+# for instance in ec2.instances.filter(Filters=[{
+#     'Name': 'availability-zone',
+#     'Values': ['us-east-1e']
+# # }]):
+#     print("Instance id is {} and Instance type is {} ".format(instance.instance_id, instance.instance_type))
+#     # Instance id is i-0bed83164f56d3eb9 and Instance type is t2.micro 
+
+ec2.instances.filter(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['terminated']
+}]).stop()
