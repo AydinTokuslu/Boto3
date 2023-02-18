7d63416cf9a39e876c8b0163dc811ee52e34533b
aydin <aydintokuslu78@gmail.com>
2023-02-18 11:39:07 +0300

    18.02.23

--- start-stop-terminate-ec2.py
+++ start-stop-terminate-ec2.py
@@ -1,1 +1,11 @@
 import boto3
+
+client = boto3.client("ec2")
+
+#client.start_instances(InstanceIds=['i-0118d05208d143c4c'])
+#client.stop_instances(InstanceIds=['i-0118d05208d143c4c'])
+#client.terminate_instances(InstanceIds=['i-0118d05208d143c4c'])
+resp = client.terminate_instances(InstanceIds=['i-0118d05208d143c4c'])
+
+for instance in resp["TerminatingInstances"]:
+    print("The instance with id {} Teminated".format(instance["InstanceId"]))
