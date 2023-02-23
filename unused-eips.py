1f68fdc6ba7e2ffc0a938a17e7529b9c2900d0db
aydin <aydintokuslu78@gmail.com>
2023-02-23 19:39:57 +0300

    23.02.23

--- unused-eips.py
+++ unused-eips.py
@@ -4,8 +4,8 @@ import os
 ec2_client = boto3.client('ec2')
 ses_client = boto3.client('ses')
 
-# SOURCE_EMAIL = os.environ['SOURCE_EMAIL']
-# DEST_EMAIL = os.environ['DEST_EMAIL']
+SOURCE_EMAIL = os.environ['aydintokuslu78@gmail.com']
+DEST_EMAIL = os.environ['aydintokuslu78@gmail.com']
 
 def lambda_handler(event,context):
     response = ec2_client.describe_addresses()
@@ -16,24 +16,24 @@ def lambda_handler(event,context):
     print(unused_eips)
 
     # send email using ses
-    # sesClient.send_email(
-    #        Source = SOURCE_EMAIL,
-    #        Destination={
-    #         'ToAddresses': [
-    #             DEST_EMAIL
-    #         ]
-    #       },
-    #       Message={
-    #         'Subject': {
-    #             'Data': 'Unused  EIPS',
-    #             'Charset': 'utf-8'
-    #         },
-    #         'Body': {
-    #             'Text': {
-    #                 'Data': str(eips),
-    #                 'Charset': 'utf-8'
-    #             }
-    #         }
-    #       }
-    #     )
+    response = ses_client.send_email(
+           Source = SOURCE_EMAIL,
+           Destination={
+            'ToAddresses': [
+                DEST_EMAIL
+            ]
+          },
+          Message={
+            'Subject': {
+                'Data': 'Unused  EIPS',
+                'Charset': 'utf-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': str(eips),
+                    'Charset': 'utf-8'
+                }
+            }
+          }
+        )
         
